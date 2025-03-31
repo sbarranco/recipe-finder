23690cd86ec34f567c03c1dcec63375b2cb28631
sbarranco <sbarrancovico@gmail.com>
2025-03-31 18:05:03 +0200

    feat: enhance SearchBar component with improved layout, accessibility features, and integration of action buttons

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent],
 })
 export class SearchBarComponent implements OnInit {
   searchItems = output<string>();
   resetSearch = output<void>();
+  randomRecipe = output<void>();
+  clearList = output<void>();
 
   search = new FormControl<string>('', [Validators.maxLength(120)]);
 
@@ -19,8 +22,8 @@ export class SearchBarComponent implements OnInit {
     this.search.valueChanges
       .pipe(debounceTime(300), distinctUntilChanged())
       .subscribe((query) => {
-        if (query !== null && query.trim() !== '') {
-          this.searchItems.emit(query);
+        if (query?.trim()) {
+          this.searchItems.emit(query.trim());
         } else {
           this.resetSearch.emit();
         }
@@ -30,5 +33,14 @@ export class SearchBarComponent implements OnInit {
   onClickResetSearch(): void {
     this.search.reset();
     this.resetSearch.emit();
+  }
+
+  onClickRandomRecipe(): void {
+    this.randomRecipe.emit();
+  }
+
+  onClickClearList(): void {
+    this.search.reset();
+    this.clearList.emit();
   }
 }
