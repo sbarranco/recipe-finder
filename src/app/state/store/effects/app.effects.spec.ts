d897f2e6d1b0e1be3ccdc6140aeb71c70e47e380
sbarranco <sbarrancovico@gmail.com>
2025-03-31 19:30:38 +0200

    feat: enhance AppEffects tests with additional actions, mock services, and navigation handling

--- src/app/state/store/effects/app.effects.spec.ts
+++ src/app/state/store/effects/app.effects.spec.ts
@@ -1,97 +1,181 @@
-// import { TestBed } from '@angular/core/testing';
-// import { provideMockActions } from '@ngrx/effects/testing';
-// import { Observable, of, throwError } from 'rxjs';
-// import { AppEffects } from './app.effects';
-// import { RecipeService } from '../../../services/recipe.service';
-// import * as fromItemActions from '../actions/app.actions';
-// import { Recipe } from '../../models/item.model';
-// import { Action } from '@ngrx/store';
-// import { createMockItemList } from '../../models/__mocks__/item.model.mock';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Observable, of, throwError } from 'rxjs';
+import { AppEffects } from './app.effects';
+import { RecipeService } from '../../../services/recipe.service';
+import * as fromRecipeActions from '../actions/app.actions';
+import { Action } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { mockRecipe } from '../../models/__mocks__/recipe.model.mock';
 
-// describe('AppEffects', () => {
-//   let actions$: Observable<Action>;
-//   let effects: AppEffects;
-//   let recipeService: jest.Mocked<RecipeService>;
+describe('AppEffects', () => {
+  let actions$: Observable<Action>;
+  let effects: AppEffects;
+  let recipeService: jest.Mocked<RecipeService>;
+  let router: jest.Mocked<Router>;
 
-//   beforeEach(() => {
-//     const RecipeServiceMock = {
-//       getItems: jest.fn(),
-//       searchItems: jest.fn(),
-//     };
+  beforeEach(() => {
+    const RecipeServiceMock = {
+      getRandomRecipe: jest.fn(),
+      searchRecipes: jest.fn(),
+      getRecipeDetails: jest.fn(),
+    };
 
-//     TestBed.configureTestingModule({
-//       providers: [
-//         AppEffects,
-//         provideMockActions(() => actions$),
-//         { provide: RecipeService, useValue: RecipeServiceMock },
-//       ],
-//     });
+    const RouterMock = {
+      navigate: jest.fn(),
+    };
 
-//     effects = TestBed.inject(AppEffects);
-//     recipeService = TestBed.inject(RecipeService) as jest.Mocked<RecipeService>;
-//   });
+    TestBed.configureTestingModule({
+      providers: [
+        AppEffects,
+        provideMockActions(() => actions$),
+        { provide: RecipeService, useValue: RecipeServiceMock },
+        { provide: Router, useValue: RouterMock },
+      ],
+    });
 
-//   describe('loadItems$', () => {
-//     it('should return a loadItemsSuccess action with items on success', () => {
-//       const items: Recipe[] = createMockItemList();
-//       const action = fromItemActions.LoadItemsActions.loadItems();
-//       const outcome = fromItemActions.LoadItemsActions.loadItemsSuccess({
-//         items
-//       });
+    effects = TestBed.inject(AppEffects);
+    recipeService = TestBed.inject(RecipeService) as jest.Mocked<RecipeService>;
+    router = TestBed.inject(Router) as jest.Mocked<Router>;
+  });
 
-//       actions$ = of(action);
-//       recipeService.getItems.mockReturnValue(of({ items }));
+  describe('loadView$', () => {
+    it('should dispatch loadRandomRecipeSuccess on success', () => {
+      const action =
+        fromRecipeActions.LoadRandomRecipeActions.loadRandomRecipe();
+      const outcome =
+        fromRecipeActions.LoadRandomRecipeActions.loadRandomRecipeSuccess({
+          recipe: [mockRecipe],
+        });
 
-//       effects.loadItems$.subscribe((result) => {
-//         expect(result).toEqual(outcome);
-//         expect(recipeService.getItems).toHaveBeenCalledWith(5, 0);
-//       });
-//     });
+      actions$ = of(action);
+      recipeService.getRandomRecipe.mockReturnValue(of([mockRecipe]));
 
-//     it('should return a loadItemsFailure action on failure', () => {
-//       const action = fromItemActions.LoadItemsActions.loadItems();
-//       const outcome = fromItemActions.LoadItemsActions.loadItemsFailure();
+      effects.loadView$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.getRandomRecipe).toHaveBeenCalled();
+      });
+    });
 
-//       actions$ = of(action);
-//       recipeService.getItems.mockReturnValue(throwError(new Error('Error')));
+    it('should dispatch loadRandomRecipeFailure on failure', () => {
+      const action =
+        fromRecipeActions.LoadRandomRecipeActions.loadRandomRecipe();
+      const outcome =
+        fromRecipeActions.LoadRandomRecipeActions.loadRandomRecipeFailure({
+          error: 'Error',
+        });
 
-//       effects.loadItems$.subscribe((result) => {
-//         expect(result).toEqual(outcome);
-//         expect(recipeService.getItems).toHaveBeenCalledWith(5, 0);
-//       });
-//     });
-//   });
+      actions$ = of(action);
+      recipeService.getRandomRecipe.mockReturnValue(
+        throwError(new Error('Error'))
+      );
 
-//   describe('searchItems$', () => {
-//     it('should return a searchItemsSuccess action with items on success', () => {
-//       const items: Recipe[] = createMockItemList();
-//       const query = 'Recipe 1';
-//       const action = fromItemActions.SearchItemsActions.searchItems({ query });
-//       const outcome = fromItemActions.SearchItemsActions.searchItemsSuccess({
-//         items,
-//       });
+      effects.loadView$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.getRandomRecipe).toHaveBeenCalled();
+      });
+    });
+  });
 
-//       actions$ = of(action);
-//       recipeService.searchItems.mockReturnValue(of({ items }));
+  describe('searchItems$', () => {
+    it('should dispatch searchRecipeSuccess on success', () => {
+      const query = 'Test';
+      const action = fromRecipeActions.SearchRecipeActions.searchRecipe({
+        query,
+      });
+      const outcome = fromRecipeActions.SearchRecipeActions.searchRecipeSuccess(
+        {
+          recipe: [mockRecipe],
+        }
+      );
 
-//       effects.searchItems$.subscribe((result) => {
-//         expect(result).toEqual(outcome);
-//         expect(recipeService.searchItems).toHaveBeenCalledWith(query);
-//       });
-//     });
+      actions$ = of(action);
+      recipeService.searchRecipes.mockReturnValue(of([mockRecipe]));
 
-//     it('should return a searchItemsFailure action on failure', () => {
-//       const query = 'Recipe 1';
-//       const action = fromItemActions.SearchItemsActions.searchItems({ query });
-//       const outcome = fromItemActions.SearchItemsActions.searchItemsFailure();
+      effects.searchItems$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.searchRecipes).toHaveBeenCalledWith(query);
+      });
+    });
 
-//       actions$ = of(action);
-//       recipeService.searchItems.mockReturnValue(throwError(new Error('Error')));
+    it('should dispatch searchRecipeFailure on failure', () => {
+      const query = 'Test';
+      const action = fromRecipeActions.SearchRecipeActions.searchRecipe({
+        query,
+      });
+      const outcome = fromRecipeActions.SearchRecipeActions.searchRecipeFailure(
+        { error: 'Error' }
+      );
 
-//       effects.searchItems$.subscribe((result) => {
-//         expect(result).toEqual(outcome);
-//         expect(recipeService.searchItems).toHaveBeenCalledWith(query);
-//       });
-//     });
-//   });
-// });
+      actions$ = of(action);
+      recipeService.searchRecipes.mockReturnValue(
+        throwError(new Error('Error'))
+      );
+
+      effects.searchItems$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.searchRecipes).toHaveBeenCalledWith(query);
+      });
+    });
+  });
+
+  describe('getRecipeById$', () => {
+    it('should dispatch getRecipeDetailsSuccess on success', () => {
+      const recipeId = '12345';
+      const action = fromRecipeActions.GetRecipeDetailsActions.getRecipeDetails(
+        { id: recipeId }
+      );
+      const outcome =
+        fromRecipeActions.GetRecipeDetailsActions.getRecipeDetailsSuccess({
+          recipe: mockRecipe,
+        });
+
+      actions$ = of(action);
+      recipeService.getRecipeDetails.mockReturnValue(of(mockRecipe));
+
+      effects.getRecipeById$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.getRecipeDetails).toHaveBeenCalledWith(recipeId);
+      });
+    });
+
+    it('should dispatch getRecipeDetailsFailure on failure', () => {
+      const recipeId = '12345';
+      const action = fromRecipeActions.GetRecipeDetailsActions.getRecipeDetails(
+        { id: recipeId }
+      );
+      const outcome =
+        fromRecipeActions.GetRecipeDetailsActions.getRecipeDetailsFailure({
+          error: 'Error',
+        });
+
+      actions$ = of(action);
+      recipeService.getRecipeDetails.mockReturnValue(
+        throwError(new Error('Error'))
+      );
+
+      effects.getRecipeById$.subscribe((result) => {
+        expect(result).toEqual(outcome);
+        expect(recipeService.getRecipeDetails).toHaveBeenCalledWith(recipeId);
+      });
+    });
+  });
+
+  describe('navigateToRecipeDetails$', () => {
+    it('should navigate to the recipe details page on success', () => {
+      const action =
+        fromRecipeActions.GetRecipeDetailsActions.getRecipeDetailsSuccess({
+          recipe: mockRecipe,
+        });
+
+      actions$ = of(action);
+
+      effects.navigateToRecipeDetails$.subscribe(() => {
+        expect(router.navigate).toHaveBeenCalledWith([
+          '/recipes',
+          mockRecipe.idMeal,
+        ]);
+      });
+    });
+  });
+});
