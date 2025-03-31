489dc784a1fc63e3246514970055c91e15d1520b
sbarranco <sbarrancovico@gmail.com>
2025-03-31 17:42:36 +0200

    feat: update RecipeCard component with improved layout, accessibility, and integration of reusable Button component

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,12 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../../state/models/recipe.model';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
 })
 export class RecipeCardComponent {
   recipe = input.required<Recipe>();
